9225a985a8ce39230ebc7b75887a785934c67a47
skillcrafters-nirali <nirali.skillcrafters@gmail.com>
2025-09-29 12:21:08 +0530

    add Header file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Header from "./component/Header/Header";
 
 function App() {
   return (
     <>
       <h1>Hello Git and Github</h1>
+      <Header />
     </>
   );
 }
