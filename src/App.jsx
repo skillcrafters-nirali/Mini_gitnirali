e0101b27e198fe442e3d8fb8e89e1ac0f078b83f
skillcrafters-nirali <nirali.skillcrafters@gmail.com>
2025-09-29 14:45:52 +0530

    Apply stashed changes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Footer from "./component/Footer/Footer";
 import Header from "./component/Header/Header";
+import Home from "./Page/Home/Home";
 
 function App() {
   return (
     <>
+      <Home />
       <Header />
       <h1>Hello Git and Github</h1>
       <Footer />
